d9bd0f3fbe2507aed447dea76db34bd4f1e3dfdd
Lucile <developer.lvt@gmail.com>
2021-03-17 15:33:56 -0400

    refactored js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 var questions = [
-    {id:1, question: "1st question", answers:["This is the first answer of q1","2","3","4","5"], correctAnswerIndex:3},
-    {id:2, question: "2nd question", answers:["This is the first answer of q2","2","3","4"], correctAnswerIndex:1},
-    {id:3, question: "3rd question", answers:["This is the first answer of q3","2","3","4"], correctAnswerIndex:1},
-    {id:4, question: "4th question", answers:["This is the first answer of q4","2","3","4"], correctAnswerIndex:2}
+  {question: "Commonly used data types DO not include ____ :", answers:["strings","booleans","alerts","numbers"], correctAnswerIndex:2},
+  {question: "The condition in an if/else statement is enclosed with ____ :", answers:["quotes","curly brackets","parenthesis","square brackets"], correctAnswerIndex:2},
+  {question: "A very useful tool used during development and debugging for printing content to the debugger is ____ :", answers:["javascript","terminal/bash","for loops","console.log"], correctAnswerIndex:3},
+  {question: "Arrays in Javascript can be used to store ____ :", answers:["numbers and strings","other arrays","booleans","all of the above"], correctAnswerIndex:3}
 ]
 
-var score = 0
 
+//initial state when page loads
 var state = 0
+var questionNumber = state-1
+$("#state").text(state)
+$("#quiz").css("display","none")
+$("#initials").css("display","none")
+$("#high-scores").css("display","none")
+$("#correction").css("display","none")
 
-var correction = ""
-
+//Independent functions
 function countdown() {
   var timeLeft = 5;
   var timeInterval = setInterval(function() {
@@ -27,60 +32,79 @@ function countdown() {
   }, 1000);
   } 
 
-function startQuiz(){
-  $("#quiz").css("display","flex")
-  $("#start-menu").css("display","none")
-  countdown();
-  state++;  
-  generateQuestion();
-  generateAnswers();
+function updateState(){
+  state++
+  $("#state").text(state)
 }
 
-
-
 function generateQuestion(){
-  $("#question").text(questions[state-1].question)
-  $("#state").text(state)  
+  if(state > questions.length){
+      $("#question").remove()
+      $("#initials").css("display","flex")
+  } else {
+      $("#question").text(questions[state-1].question)
+  }
 }
 
 function generateAnswers(){
-  for(var i=0;i<questions[state-1].answers.length;i++){
+  if(state > questions.length){
+      $("#answers").remove()
+  } else {
+      for(var i=0;i<questions[state-1].answers.length;i++){
       var answer = questions[state-1].answers[i]
-      $("#answers").append(`<button class="col-12 btn btn-outline-primary" id=${state}-${i}>${answer}</button>`)
-  }
-  checkAnswer()
-}
-
-function checkAnswer(){
-  $(`[id*=${state}-]`).each(function(index) {
-    $(this).click(function(){
-      console.log(index)
-      console.log(questions[state-1].correctAnswerIndex)
-      if(index == questions[state-1].correctAnswerIndex){
-        console.log(true)
-        state++
-        erasePreviousAnswers()
-        generateQuestion()
-        generateAnswers()
-        $("#correction").text("You answered correctly to the previous question")
-      } else {
-        state++
-        console.log(false)
-        erasePreviousAnswers()
-        generateQuestion()
-        generateAnswers()
-        $("#correction").text("You answered incorrectly to the previous question")
+      $("#answers").append(`<button class="col-12 btn btn-outline-primary" id=${state}-${i}><li>${answer}</li></button>`)
       }
-    })
-  });
+  }
 }
 
 function erasePreviousAnswers(){
   $(`[id*=${state-1}-]`).each(function() {
-    $(this).css("display","none")})
+      $(this).css("display","none")})
 }
 
 
+//Groups of functions
+function nextQuestion(){
+  updateState()
+  erasePreviousAnswers()
+  generateQuestion()
+  generateAnswers()
+}
 
+function checkAnswerAndNext() {
+  $("#answers").on("click", "button", function() {
+     var answerID = $(this).attr("id")
+     if(answerID == state + "-" + questions[state-1].correctAnswerIndex){
+         nextQuestion()
+         score++
+         $("#correction").css("display","flex")
+         $("#correction").text("You answered correctly to the previous question!")
+     } else {
+          nextQuestion()
+          $("#correction").css("display","flex")
+          $("#correction").text("You answered incorrectly to the previous question!")
+     }
+  })
+}
 
+function startQuiz(){
+  $("#start-menu").css("display","none")
+  $("#quiz").css("display","flex")
+  countdown();
+  updateState()
+  generateQuestion();
+  generateAnswers();
+  checkAnswerAndNext()
+}
+
+// Actionable buttons
 $("#start").click(function(){startQuiz()})
+
+$("#initials").submit(function(event){
+  updateState()
+  $("#initials").css("display","none")
+  $("#high-scores").css("display","flex")
+  var initials = $("#myInitials").val()
+  console.log(initials)
+  event.preventDefault()
+})
